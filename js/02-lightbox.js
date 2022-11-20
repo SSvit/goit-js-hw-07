25c09e1748537a794a861d2186920b7fdb9de40f
SSvit <108158154+SSvit@users.noreply.github.com>
2022-11-20 10:24:42 +0200

    написання завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const listTemplate = (preview, original, description) => `
+<li>
+    <a class = "gallery__item" href = "${original}">
+        <img class = "gallery__image" src = "${preview} alt = "${description}" />
+    </a>
+</li>
+`;
+
+const render = () => {
+    const arrayFromListTemplaate = galleryItems.map(item => listTemplate(item.preview, item.original, item.description));
+
+    listEl.innerHTML = arrayFromListTemplaate.join('');
+};
+render();
+
+const galleryEl = new SimpleLightbox('ul.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
